9aaffe24a76f8697a058eac666d322f106d3c66d
Sam Junior <151775725+ghsam-2408@users.noreply.github.com>
2025-08-19 18:22:26 +0200

    First commit
    School Management System

---
+++ School Management System/School Management System/LoginForm.vb
@@ -0,0 +1,6 @@
+﻿Public Class LoginForm
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MsgBox("Hello how are you")
+    End Sub
+End Class
